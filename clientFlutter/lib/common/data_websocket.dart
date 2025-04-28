00b81f03a69040168ef98e0bffff9e7197182991
EchoFD54 <144889612+EchoFD54@users.noreply.github.com>
2025-04-28 15:33:53 +0200

    pubspec fix

--- clientFlutter/lib/common/data_websocket.dart
+++ clientFlutter/lib/common/data_websocket.dart
@@ -1,15 +1,1 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'models_appointments.dart';
-
-class DataSourceWebsocket {
-
-
-}
-
-
-
-
-
-
-
+class DataSourceWebsocket {}
