c4957edebc77b480b5dd29061809f480699ff660
Meile5 <meile.kapisaite5@gmail.com>
2025-05-12 13:14:31 +0200

    Added .env

--- clientFlutter/lib/patient/overview/utility/data_source_overview.dart
+++ clientFlutter/lib/patient/overview/utility/data_source_overview.dart
@@ -1,18 +1,19 @@
 import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:medicare/common/auth/auth_prefs.dart';
 import '../models/models_overview.dart';
-
+import 'package:uri/uri.dart';
 
 class DataSourceOverview {
   Future<List<ClinicInfoDto>> retrieveClinicInfo() async {
-    final url = "http://localhost:5000/RetrieveClinicInfo";
-    final response = await http.post(
+    final url = dotenv.env['API_BASE_URL']! + "/RetrieveClinicInfo?userId=${AuthPrefs.userId}";
+    final response = await http.get(
       Uri.parse(url),
       headers: {
         'Content-Type': 'application/json', 'Authorization': AuthPrefs.jwt!,
       },
-      body: json.encode(AuthPrefs.userId),
+      //body: json.encode(AuthPrefs.userId),
     );
 
     final List<dynamic> decoded = json.decode(response.body);
