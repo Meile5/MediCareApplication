c6f41294d828ab984288a65f3e8ae6766829df63
Meile5 <meile.kapisaite5@gmail.com>
2025-05-12 13:13:14 +0200

    In backend added authorization for each controller, added controller for retrieving diagnosis.

--- server/Api.Rest/Controllers/Patient/BookingController.cs
+++ server/Api.Rest/Controllers/Patient/BookingController.cs
@@ -1,4 +1,5 @@
-﻿using Application.Interfaces.IPatientService;
+﻿using Application.Interfaces;
+using Application.Interfaces.IPatientService;
 using Application.Models.Dtos.PatientDto;
 using Application.Models.Dtos.PatientDto.request;
 using Application.Models.Dtos.PatientDto.response;
@@ -7,26 +8,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Rest.Controllers.Patient;
 [ApiController]
-public class BookingController (IBookingService _bookingService) : ControllerBase
+public class BookingController (IBookingService bookingService, ISecurityService securityService) : ControllerBase
 
 {
     public const string RetrieveBookingInfoRoute = nameof(RetrieveBookingInfo);
     
     [Route(RetrieveBookingInfoRoute)]
-    public async Task <ActionResult<AvailabilityDto> >RetrieveBookingInfo([FromBody] string doctorId) 
+    [HttpPost]
+    public async Task <ActionResult<AvailabilityDto> >RetrieveBookingInfo([FromBody] string doctorId, [FromHeader]string authorization) 
     
-    {
-       // securityService.VerifyJwtOrThrow(authorization);
-       var response = await _bookingService.RetrieveBookingInfo(doctorId);
+    { 
+        securityService.VerifyJwtOrThrow(authorization);
+       var response = await bookingService.RetrieveBookingInfo(doctorId);
         return Ok(response);
     }
 
     public const string BookAppointmentRoute = nameof(BookAppointment);
 
     [Route(BookAppointmentRoute)]
-    public async Task<ActionResult> BookAppointment([FromBody] BookAppointmentRequest dto)
+    public async Task<ActionResult> BookAppointment([FromBody] BookAppointmentRequest dto, [FromHeader]string authorization)
     {
-        await _bookingService.BookAppointment(dto.Appointment, dto.ChatRoom);
+        securityService.VerifyJwtOrThrow(authorization);
+        await bookingService.BookAppointment(dto.Appointment, dto.ChatRoom);
         
         return Ok();
     }
@@ -34,44 +37,44 @@ public class BookingController (IBookingService _bookingService) : ControllerBas
     public const string RetrieveFutureAppointmentsRoute = nameof(RetrieveFutureAppointments);
     
     [Route(RetrieveFutureAppointmentsRoute)]
-    public async Task <ActionResult<List<FutureAppointmentsDto>>> RetrieveFutureAppointments([FromBody] string userId) 
+    public async Task <ActionResult<List<FutureAppointmentsDto>>> RetrieveFutureAppointments([FromBody] string userId, [FromHeader]string authorization) 
     
     {
-        // securityService.VerifyJwtOrThrow(authorization);
-        var response = await _bookingService.RetrieveFutureAppointments(userId);
+        securityService.VerifyJwtOrThrow(authorization);
+        var response = await bookingService.RetrieveFutureAppointments(userId);
         return Ok(response);
     }
     
     public const string RetrievePastAppointmentsRoute = nameof(RetrievePastAppointments);
     
     [Route(RetrievePastAppointmentsRoute)]
-    public async Task <ActionResult<List<PastAppointmentsDto>>> RetrievePastAppointments([FromBody] string userId) 
+    public async Task <ActionResult<List<PastAppointmentsDto>>> RetrievePastAppointments([FromBody] string userId, [FromHeader]string authorization) 
     
     {
-        // securityService.VerifyJwtOrThrow(authorization);
-        var response = await _bookingService.RetrievePastAppointments(userId);
+        securityService.VerifyJwtOrThrow(authorization);
+        var response = await bookingService.RetrievePastAppointments(userId);
         return Ok(response);
     }
     
     public const string CancelAppointmentRoute = nameof(CancelAppointment);
     
     [Route(CancelAppointmentRoute)]
-    public async Task <ActionResult> CancelAppointment([FromBody] CancelAppointmentDto dto) 
+    public async Task <ActionResult> CancelAppointment([FromBody] CancelAppointmentDto dto, [FromHeader]string authorization) 
     
     {
-        // securityService.VerifyJwtOrThrow(authorization);
-        await _bookingService.CancelAppointment(dto);
+        securityService.VerifyJwtOrThrow(authorization);
+        await bookingService.CancelAppointment(dto);
         return Ok();
     }
     
     public const string RetrieveDoctorsRoute = nameof(RetrieveDoctors);
     
     [Route(RetrieveDoctorsRoute)]
-    public async Task <ActionResult<List<ClinicDoctorDto>>> RetrieveDoctors([FromBody] string clinicId) 
+    public async Task <ActionResult<List<ClinicDoctorDto>>> RetrieveDoctors([FromBody] string clinicId, [FromHeader]string authorization) 
     
     {
-        // securityService.VerifyJwtOrThrow(authorization);
-        var response = await _bookingService.RetrieveClinicDoctors(clinicId);
+        securityService.VerifyJwtOrThrow(authorization);
+        var response = await bookingService.RetrieveClinicDoctors(clinicId);
         return Ok(response);
     }
     
--- server/Api.Rest/Controllers/Patient/OverviewController.cs
+++ server/Api.Rest/Controllers/Patient/OverviewController.cs
@@ -1,21 +1,37 @@
-﻿using Application.Interfaces.IPatientService;
+﻿using Application.Interfaces;
+using Application.Interfaces.IPatientService;
 using Application.Models.Dtos.PatientDto.response;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Rest.Controllers.Patient;
 
-public class OverviewController(IOverviewService _overviewService)  : ControllerBase
+public class OverviewController(IOverviewService overviewService, ISecurityService securityService)  : ControllerBase
 {
     
     public const string RetrieveClinicInfoRoute= nameof(RetrieveClinicInfo);
     
-    [Route(RetrieveClinicInfoRoute)]
-    public async Task <ActionResult<List<ClinicInfoDto>>>RetrieveClinicInfo([FromBody] string userId) 
+    [Route(RetrieveClinicInfoRoute)
+    ]
+    [HttpGet]
+    public async Task <ActionResult<List<ClinicInfoDto>>>RetrieveClinicInfo([FromQuery] string userId, [FromHeader]string authorization) 
     
     {
-        // securityService.VerifyJwtOrThrow(authorization);
-        var response = await _overviewService.RetrieveClinicInfo(userId);
+        securityService.VerifyJwtOrThrow(authorization);
+        var response = await overviewService.RetrieveClinicInfo(userId);
         return Ok(response);
     }
+    
+    public const string RetrieveDiagnosesRoute= nameof(RetrieveDiagnoses);
+    [Route(RetrieveDiagnosesRoute)]
+    public async Task <ActionResult<List<ClinicInfoDto>>>RetrieveDiagnoses([FromBody] string userId, [FromHeader]string authorization) 
+    
+    {
+        securityService.VerifyJwtOrThrow(authorization);
+        var response = await overviewService.RetrieveClinicInfo(userId);
+        return Ok(response);
+    }
+    
+    
+
     
 }
