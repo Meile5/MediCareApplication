eb8baa3dad35cf087ad11ede36b08f8d1f74b106
EchoFD54 <144889612+EchoFD54@users.noreply.github.com>
2025-05-26 13:20:14 +0200

    unsubscribe from doctor room on calendar

--- clientFlutter/lib/doctor/appointment/state/doctor_appointment_cubit.dart
+++ clientFlutter/lib/doctor/appointment/state/doctor_appointment_cubit.dart
@@ -3,6 +3,7 @@ import 'dart:developer' as developer;
 import 'dart:io';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:medicare/common/auth/auth_prefs.dart';
 import 'package:medicare/doctor/appointment/models/appointment_model.dart';
 
 import '../../../common/event_models/events.dart';
@@ -127,6 +128,21 @@ class DoctorAppointmentCubit extends Cubit<DoctorAppointmentState> {
         ),
       );
     }
+  }
+
+  void joinRoom(String roomId) {
+    if (_isSubscribed) return;
+
+    _isSubscribed = true;
+    webSocketService.send(
+      JoinRoom(roomId: roomId, token: AuthPrefs.jwt).toJson(),
+    );
+    print('Subscribed to Room: $roomId');
+  }
+
+  void unsubscribeFromRoom(String roomId) {
+    webSocketService.send(UnsubscribeFromChat(roomId: roomId).toJson());
+    print('Unsubscribed from Room: $roomId');
   }
 
   @override
