f6eca6c0b3daf032bacb963b8add755460f6aa11
EchoFD54 <144889612+EchoFD54@users.noreply.github.com>
2025-05-27 21:57:23 +0000

    Deploying to gh-pages from @ Meile5/MediCareApplication@46ae479c747634f81ab6246688b6a81103dd1914 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "canvaskit/chromium/canvaskit.js": "8191e843020c832c9cf8852a4b909d4c",
 "canvaskit/chromium/canvaskit.wasm": "c054c2c892172308ca5a0bd1d7a7754b",
 "canvaskit/chromium/canvaskit.js.symbols": "f7c5e5502d577306fb6d530b1864ff86",
-"flutter_bootstrap.js": "b376945c2f9643fd19d7b2d584c43c01",
+"flutter_bootstrap.js": "af38ab6940f0b1fe749da2780d9a0f02",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "386a54827e7b3222047acbf070b15219",
 "/": "386a54827e7b3222047acbf070b15219",
