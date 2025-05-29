3b29124a92ba1da1196396928d472b50292b3709
Meile5 <144432344+Meile5@users.noreply.github.com>
2025-05-29 20:53:53 +0000

    Deploying to gh-pages from @ Meile5/MediCareApplication@48182fe1deac0f83ec217230c5042d2d2ca85a09 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2354189027"
+    serviceWorkerVersion: "464493450"
   }
 });
