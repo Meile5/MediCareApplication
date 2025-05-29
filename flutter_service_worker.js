2f72bd5e2f3543db59f19d903ca3bb48ff1ed4fb
Meile5 <144432344+Meile5@users.noreply.github.com>
2025-05-29 21:08:43 +0000

    Deploying to gh-pages from @ Meile5/MediCareApplication@c589603a9dd1f1a32335030d08e1c728607477d9 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "canvaskit/chromium/canvaskit.js": "8191e843020c832c9cf8852a4b909d4c",
 "canvaskit/chromium/canvaskit.wasm": "c054c2c892172308ca5a0bd1d7a7754b",
 "canvaskit/chromium/canvaskit.js.symbols": "f7c5e5502d577306fb6d530b1864ff86",
-"flutter_bootstrap.js": "6bcb3830b565ad2e223e67cdb99bc665",
+"flutter_bootstrap.js": "1b0e9eee48c56190feeae8f1f6904db0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "386a54827e7b3222047acbf070b15219",
 "/": "386a54827e7b3222047acbf070b15219",
